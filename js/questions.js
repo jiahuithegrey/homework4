fc4ae3b46600e75cf7f163ee978c82380d2d0870
Jiahui Wang <wangjiahui@WangJiahuis-MacBook-Pro.local>
2019-10-09 00:15:38 -0700

    landing page html done

---
+++ js/questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        title: "Which Defence Against the Dark Arts teacher filled his office with pictures of himself?",
+        choices: ["Professor Lupin", "Professor Lockhart", "Professor Snape", "Professor Moody"],
+        answer: "Professor Lockhart"
+    },
+    {
+        title: "How many known secret passages lead from Hogwarts to Hogsmeade?",
+        choices: ["6", "7", "8", "9"],
+        answer: "7"
+    },
+    {
+        title: "Which subject at Hogwarts is taught by Professor Binns?",
+        choices: ["Ancient Runes", "Muggle Studies", "Arithmancy", "History of Magic"],
+        answer: "History of Magic"
+      },
+    {
+        title: "Who was the first student in Harry’s year to be sorted into Gryffindor?",
+        choices: ["Lavender Brown", "Susan Bones", "Mandy Brocklehurst", "Hannah Abbott"],
+        answer: "Lavender Brown"
+    },
+    {
+        title: "How many staircases are there at Hogwarts?",
+        choices: ["218", "96", "312", "142"],
+        answer: "142"
+    }
+  ];
+  
+  
+  * The length of the array in `questions.js` determines the length of play. 
+  Fifteen seconds per question is a good estimate, so 5 questions will result in a length of play of 75 seconds.
+  
