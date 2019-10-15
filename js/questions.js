6d2a43828a701fc90f3d314f51a80d0251881ca3
Jiahui Wang <wangjiahui@WangJiahuis-MacBook-Pro.local>
2019-10-15 14:15:33 -0700

    js not fully functioning, skips 3rd quesion; timer goes to negative...

--- js/questions.js
+++ js/questions.js
@@ -1,5 +1,10 @@
 var questions = [
     {
+        title: "Which subject at Hogwarts is taught by Professor Binns?",
+        choices: ["Ancient Runes", "Muggle Studies", "Arithmancy", "History of Magic"],
+        answer: "History of Magic"
+    },
+    {
         title: "Which Defence Against the Dark Arts teacher filled his office with pictures of himself?",
         choices: ["Professor Lupin", "Professor Lockhart", "Professor Snape", "Professor Moody"],
         answer: "Professor Lockhart"
@@ -9,11 +14,6 @@ var questions = [
         choices: ["6", "7", "8", "9"],
         answer: "7"
     },
-    {
-        title: "Which subject at Hogwarts is taught by Professor Binns?",
-        choices: ["Ancient Runes", "Muggle Studies", "Arithmancy", "History of Magic"],
-        answer: "History of Magic"
-      },
     {
         title: "Who was the first student in Harry’s year to be sorted into Gryffindor?",
         choices: ["Lavender Brown", "Susan Bones", "Mandy Brocklehurst", "Hannah Abbott"],
@@ -27,61 +27,58 @@ var questions = [
 ];
 // As soon as the question starts, timer begins
 var timeInterval = setTime();
+var timerEl = document.querySelector("#timer-number");
 
 var questionEl = document.querySelector("#question");
-var choiceTextEl = document.querySelectorAll(".choice-text");
-var buttonEl = document.querySelectorAll(".choice-text");
+var choiceEl = document.querySelectorAll(".buttonName");
+var buttonEl = document.querySelectorAll(".choiceBtn");
 
 var score = 0;
 var qIndex = 0;
-
 var alertEl = document.querySelector("#alert");
 
 function nextQuestion(){
     questionEl.textContent = questions[qIndex].title;
 
     for (var i=0; i<questions[qIndex].choices.length; i++){
-        choiceTextEl[i].textContent = questions[qIndex].choices[i]; 
+        choiceEl[i].textContent = questions[qIndex].choices[i]; 
 
         buttonEl[i].addEventListener("click", function(event){
             var userChoice = event.target.textContent;
             if (userChoice === questions[qIndex].answer){
-                // alert("right");
                 alertEl.textContent = "Correct!";
                 score+=10;
             }else{
-                // alert("wrong");
                 alertEl.textContent = "Wrong!";
-                // secondsLeft = secondsLeft-15;
+                secondsLeft = secondsLeft-15;
             }
-    
+
             if (qIndex<questions.length){
-            qIndex++;
-            }else{
-                clearInterval(timerInterval);
-                alert("Game Over! Your total score is " + score);
-            }
-            arrayQues.innerHTML ="";
-            return nextQuestion();
-        })
+                qIndex++;
+                }else{
+                    clearInterval(timerInterval);
+                    alert("Game Over! Your total score is " + score);
+                }
+                //buttonEl.innerHTML ="";
+                return nextQuestion();
+        });
     }
 }
-    nextQuestion();
+nextQuestion();
 
-    var timerEl = document.querySelector("#timer");
     var secondsLeft=75;
 
     function setTime(){
         var timeInterval = setInterval(function(){
             secondsLeft--;
-            timeEl.textContent=secondsLeft;
+            timerEl.textContent=secondsLeft;
 
-            if(secondsLeft ===0){
+            if(secondsLeft === 0){
                 clearInterval(timerInterval);
                 sendMessage();
             }
         }, 1000);
-            return timeInterval;
+        return timeInterval;
     }
 
     function sendMessage(){
